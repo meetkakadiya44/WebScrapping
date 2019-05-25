2e51f2c6b465ca0eab9e93b1996bfe513db9bfbf
meetkakadiya44 <47652924+meetkakadiya44@users.noreply.github.com>
2019-05-25 13:27:18 +0530

    Update med.py

--- med.py
+++ med.py
@@ -16,20 +16,28 @@ import string
 
 
 
-#rejecting forbidden error
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+#-------------------------------------------GETTING THE URL AND SOUPIFYING THE LINK SENT--------------------------------------------------------------------------------
 
-url = "https://www.netmeds.com/prescriptions"
-headers={'User-Agent':user_agent,} 
+def make_soup(url):
 
-request=urllib.request.Request(url,None,headers) #The assembled request
-thepage = urllib.request.urlopen(request)
-#passing to s
-soup=bs(thepage,"html.parser")
+    #rejecting forbidden error
+
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers={'User-Agent':user_agent,} 
+    
+    request=urllib.request.Request(url,None,headers) #The assembled request
+    thepage = urllib.request.urlopen(request)
+    #passing to s
+    soup=bs(thepage,"html.parser")
+    return soup
+#---------------------------------------------------------------------------------------------------------------------------
 
 
-
+#getting website here////////////////////////-----------------
+url1= "https://www.netmeds.com/prescriptions"
+#/////////////////////////////////////////---------------------
+soup1=make_soup(url1)
 
 
 
@@ -40,9 +48,9 @@ soup=bs(thepage,"html.parser")
 j=0
 names=[]
 while(j<20):
-    ass= soup.find('div',{'id': 'prescriptions_products'}).findAll("ul", {"class" : "alpha-drug-list"})[j]
+    ss= soup1.find('div',{'id': 'prescriptions_products'}).findAll("ul", {"class" : "alpha-drug-list"})[j]
             
-    for i in ass.findAll('a'):
+    for i in ss.findAll('a'):
         #print(i.text)
         names.append(i.text)
         
@@ -75,7 +83,7 @@ for i in range(len(list1)):
         
 
 # this code removes the last space from all the elements of list1 and append it in 'reallist' list
-#  i did this because i was unable to use 'rstrip' function          
+#  i did this because i was unable to use 'rstrip' function, "smh!"      
     
 reallist=[]
     
@@ -86,7 +94,7 @@ for i in list1:
 
     
     
-#let's solve the error situatiomn of anaesthesia-local
+#let's solve the error situation of anaesthesia-local
 
 reallist[7]="Anaesthesia-Local"
     
@@ -108,13 +116,55 @@ list with name 'reallist' is prepared, which can be used to iterate all the webs
 '''
 #--------------------------------------------------------------------------------------------------------------------------------------
   #lowercasing all the characters so that it can load the list faster
-string1= "omemfMFMEFMOcmmffkf"      
+     
 for i in range(len(reallist)):
     reallist[i]=reallist[i].lower()
         
-print(reallist)    
+#print(reallist)    
 #---------------------------------------------------------------------------------------------------------   
-        
+#this list contains all the diseases which are to be traveresed to get all The medicines
+
+
+
+'''
+ below code is used to traverse every website using the list 'reallist' 
+     and then we'll just open every 'href' there is and open it
+     and would open the webpage through that link obtained and extract the data of name and price
+     which is yet easy said then done.
+     
+'''
+
+#-------------------BELOW CODE IS USED TO TRAVERSE EVERY DISEASES AND MEDICINES CONTAINED IN EACH DIESEASE-----------------------------
+s=[]
+
+p=0
+for i in range(len(reallist)):
+    s=make_soup("https://www.netmeds.com/prescriptions/"+reallist[i] )
+    try:
+        while(p<26):
+            ac= s.find('div', {'id' : 'prescriptions_products'}).findAll("ul", {"class" : "alpha-drug-list"})[p]
+           
+            for j in ac.findAll('a',href=True):
+                print(j['href'])
+            
+            p=p+1
+    except:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         
         
